70ef40daaa03316f8425290fee2508569af18886
Fotios Zachopoulos <65657774+zachfotis@users.noreply.github.com>
2022-12-18 18:52:50 +0200

    change address api responses to full address

--- controllers/api.js
+++ controllers/api.js
@@ -4,7 +4,7 @@ const getPlaces = async (req, res) => {
   const input = req.query.address;
 
   const options = {
-    type: 'street_address',
+    type: 'address',
     input: input,
     language: 'el',
     key: process.env.GOOGLE_PLACES_API_KEY,
@@ -26,14 +26,25 @@ const getCoordinatesFromAddress = async (req, res) => {
 
   const options = {
     address: input,
+    language: 'el',
     key: process.env.GOOGLE_PLACES_API_KEY,
   };
 
   const url = `https://maps.googleapis.com/maps/api/geocode/json?${new URLSearchParams(options)}`;
   const response = await axios.get(url);
 
+  if (response.data.status === 'ZERO_RESULTS') {
+    return res.json(null);
+  }
+
   if (response?.data?.results[0]?.geometry?.location) {
-    return res.json(response.data.results[0].geometry.location);
+    const coordinates = response.data.results[0].geometry.location;
+    const fullAddress = {
+      ...addressComponentsToAddress(response.data.results[0].address_components),
+      ...coordinates,
+    };
+
+    return res.json(fullAddress);
   } else {
     return res.json(null);
   }
@@ -52,11 +63,51 @@ const getAddressFromCoordinates = async (req, res) => {
   const url = `https://maps.googleapis.com/maps/api/geocode/json?${new URLSearchParams(options)}`;
   const response = await axios.get(url);
 
-  if (response?.data?.results[0]?.formatted_address) {
-    return res.json(response.data.results[0].formatted_address);
+  if (response.data.status === 'ZERO_RESULTS') {
+    return res.json(null);
+  }
+
+  if (response?.data?.results[0]?.geometry?.location) {
+    const coordinates = response.data.results[0].geometry.location;
+    const fullAddress = {
+      ...addressComponentsToAddress(response.data.results[0].address_components),
+      ...coordinates,
+    };
+
+    return res.json(fullAddress);
   } else {
     return res.json(null);
   }
+};
+
+const addressComponentsToAddress = (addressComponents) => {
+  const address = addressComponents
+    .find((component) => {
+      return component.types.includes('route');
+    })
+    ?.long_name?.trim();
+
+  const number = addressComponents
+    .find((component) => {
+      return component.types.includes('street_number');
+    })
+    ?.long_name?.trim();
+
+  const area = addressComponents.find((component) => component.types.includes('locality'))?.long_name?.trim();
+
+  const city = addressComponents
+    .find((component) => component.types.includes('administrative_area_level_3'))
+    ?.long_name?.trim();
+
+  const postalCode = addressComponents.find((component) => component.types.includes('postal_code'))?.long_name?.trim();
+
+  return {
+    address,
+    number,
+    area,
+    city,
+    postalCode,
+  };
 };
 
 export default {
